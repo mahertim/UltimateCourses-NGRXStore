8d90e9e0ccbc19d7ce78ec0ecef1246ae1852da3
Tim Maher <tim@timmaher.dev>
2020-01-21 09:45:28 -0500

    Dispatching Actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -10,4 +10,12 @@ export class Store {
   get value() {
     return this.state;
   }
+
+  dispatch(action: { type: string; payload: any }) {
+    this.state = {
+      ...this.state,
+      todos: [...this.state.todos, action.payload],
+    };
+    console.log(this.state);
+  }
 }
