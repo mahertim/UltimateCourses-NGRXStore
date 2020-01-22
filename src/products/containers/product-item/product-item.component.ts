9247b4bb5c9fd1b001a5e9ef3b3863f041e41271
Tim Maher <tim@timmaher.dev>
2020-01-22 10:37:15 -0500

    Router State and Entity Composition

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit /* ChangeDetectionStrategy */ } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+
+import { Observable, of } from 'rxjs';
+
+import * as fromStore from '../../store';
 
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
-
 import { Topping } from '../../models/topping.model';
-import { ToppingsService } from '../../services/toppings.service';
 
 @Component({
   selector: 'app-product-item',
@@ -13,66 +15,25 @@ import { ToppingsService } from '../../services/toppings.service';
   templateUrl: 'product-item.component.html',
 })
 export class ProductItemComponent implements OnInit {
-  pizza: Pizza = {};
+  pizza$: Observable<Pizza> = of({});
   visualize: Pizza = {};
   toppings: Topping[] = [];
 
-  constructor(
-    private pizzaService: PizzasService,
-    private toppingsService: ToppingsService,
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {}
+  constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe((pizzas) => {
-      const param = this.route.snapshot.params.id;
-      let pizza;
-      if (param === 'new') {
-        pizza = {};
-      } else {
-        pizza = pizzas.find((thePizza) => thePizza.id === parseInt(param, 10));
-      }
-      this.pizza = pizza ? pizza : {};
-      this.toppingsService.getToppings().subscribe((toppings) => {
-        this.toppings = toppings;
-        this.onSelect(
-          toppings.map((topping) => (topping.id ? topping.id : NaN)),
-        );
-      });
-    });
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
   }
 
-  onSelect(event: number[]) {
-    let toppings;
-    if (this.toppings && this.toppings.length) {
-      toppings = event.map((id) =>
-        this.toppings.find((topping) => topping.id === id),
-      );
-    } else {
-      toppings = this.pizza.toppings;
-    }
-    this.visualize = { ...this.pizza, toppings: toppings as Topping[] };
-  }
+  onSelect(_event: number[]) {}
 
-  onCreate(event: Pizza) {
-    this.pizzaService.createPizza(event).subscribe((pizza) => {
-      this.router.navigate([`/products/${pizza.id}`]);
-    });
-  }
+  onCreate(_event: Pizza) {}
 
-  onUpdate(event: Pizza) {
-    this.pizzaService.updatePizza(event).subscribe(() => {
-      this.router.navigate([`/products`]);
-    });
-  }
+  onUpdate(_event: Pizza) {}
 
-  onRemove(event: Pizza) {
+  onRemove(_event: Pizza) {
     const remove = window.confirm('Are you sure?');
     if (remove) {
-      this.pizzaService.removePizza(event).subscribe(() => {
-        this.router.navigate([`/products`]);
-      });
     }
   }
 }
--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,8 +1,4 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-} from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 import { PizzasAction } from '../actions/pizzas.action';
@@ -20,29 +16,4 @@ export const reducers: ActionReducerMap<ProductsState, ProductsAction> = {
 
 export const getProductsState = createFeatureSelector<ProductsState>(
   'products',
-);
-
-// pizza state
-export const getPizzaState = createSelector(
-  getProductsState,
-  (state: ProductsState) => state.pizzas,
-);
-
-export const getPizzasEntities = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasEntities,
-);
-
-export const getAllPizzas = createSelector(getPizzasEntities, (entities) => {
-  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
-});
-
-export const getPizzasLoaded = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoaded,
-);
-
-export const getPizzasLoading = createSelector(
-  getPizzaState,
-  fromPizzas.getPizzasLoading,
 );
