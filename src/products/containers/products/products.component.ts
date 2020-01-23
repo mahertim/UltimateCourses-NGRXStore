f6964fd3a27084204e53871769a114c3d74f63cd
Tim Maher <tim@timmaher.dev>
2020-01-23 13:15:57 -0500

    Preloading across multiple Routes

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -19,7 +19,6 @@ export class ProductsComponent implements OnInit {
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.store.dispatch(new fromStore.LoadPizzas());
     this.pizzas$ = this.store.select<Pizza[]>(fromStore.getAllPizzas);
     this.store.dispatch(new fromStore.LoadToppings());
   }
